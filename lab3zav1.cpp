0ca908846bcaffc85432e2365243ec4ebaadf4da
Viktoria157 <epik190119@gmail.com>
2025-10-19 23:23:54 +0300

    Add files via upload

---
+++ lab3zav1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double f, a = 4, b = 3.2, c = 0.1;
+    f = tan(c) - sqrt(c / b + a);
+
+    cout << "f = " << f << endl;
+
+    return 0;
+}
